ad442e4df6e1ad743beb3e5e5d9ec713bbca56c8
GPDario <d.a.guzman2410@gmail.com>
2023-11-24 12:04:33 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DarkStore.V.2.0',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
